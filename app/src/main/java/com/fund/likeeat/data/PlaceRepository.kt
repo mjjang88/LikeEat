0c60a6bb4e9df6fa16628fc1ae8b950edd2188e1
DT_036665 <DT036665@mobis.co.kr>
2020-07-23 13:38:07 +0900

    import koin library DI 라이브러리인 Koin을 도입함

--- app/src/main/java/com/fund/likeeat/data/PlaceRepository.kt
+++ app/src/main/java/com/fund/likeeat/data/PlaceRepository.kt
@@ -2,19 +2,10 @@ package com.fund.likeeat.data
 
 import androidx.lifecycle.LiveData
 
-class PlaceRepository private constructor(
+class PlaceRepository (
     private val placeDao: PlaceDao
 ){
     fun getPlaceList(): LiveData<List<Place>> {
         return placeDao.getPlaceList()
-    }
-
-    companion object {
-        @Volatile private var instance: PlaceRepository? = null
-
-        fun getInstance(placeDao: PlaceDao) =
-            instance ?: synchronized(this) {
-                instance ?: PlaceRepository(placeDao).also { instance = it }
-            }
     }
 }
