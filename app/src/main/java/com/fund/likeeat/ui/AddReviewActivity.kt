9e719f0768d11bf0d48a8f7ca03a2e9765d2a918
mjjang <inseok16@gmail.com>
2020-08-17 22:07:37 +0900

    레이아웃 작성

---
+++ app/src/main/java/com/fund/likeeat/ui/AddReviewActivity.kt
@@ -0,0 +1,11 @@
+package com.fund.likeeat.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddReviewActivity : AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
