b604c13b3db2bdece4173c5e850ddc602e28e051
LeeDongHyun <linear14@naver.com>
2020-09-23 11:46:35 +0900

    Left Drawer UI + 카카오 계정 정보 불러오기
    다른 작업 먼저 하기 위해 COMMIT

--- app/src/main/java/com/fund/likeeat/ui/MapFragment.kt
+++ app/src/main/java/com/fund/likeeat/ui/MapFragment.kt
@@ -4,13 +4,17 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.observe
+import com.bumptech.glide.Glide
 import com.fund.likeeat.R
 import com.fund.likeeat.adapter.ReviewsAdapter
 import com.fund.likeeat.databinding.FragmentMapBinding
@@ -18,14 +22,18 @@ import com.fund.likeeat.manager.MyApplication
 import com.fund.likeeat.manager.PermissionManager
 import com.fund.likeeat.utilities.GpsTracker
 import com.fund.likeeat.utilities.INTENT_KEY_LOCATION
+import com.fund.likeeat.utilities.ToastUtil
 import com.fund.likeeat.viewmodels.MapViewModel
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.kakao.sdk.user.UserApiClient
+import com.kakao.usermgmt.response.model.User
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.OverlayImage
 import kotlinx.android.synthetic.main.fragment_map.*
+import kotlinx.android.synthetic.main.navigation_left.view.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
@@ -82,9 +90,15 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             moveToPoi(LatLng(gpsTracker.getLatitude(), gpsTracker.getLongitude()))
         }
 
-        binding.testButton.setOnClickListener {
+        binding.navigationLeft.all_theme_layout.setOnClickListener {
             val intent = Intent(requireContext(), ThemeActivity::class.java)
             startActivity(intent)
+            drawer.closeDrawer(GravityCompat.START)
+        }
+
+        binding.buttonLeftDrawer.setOnClickListener {
+            dataInit()  //TODO 현재 로그인 안 된 상태에서도 MapFragment 진입하기 때문에 오류가 발생함 (그렇게 유추하고 있음). 해결되면 바로 밖으로 빼버리자
+            drawer.openDrawer(GravityCompat.START)
         }
 
         return binding.root
@@ -206,5 +220,27 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                 mNaverMap.cameraPosition = CameraPosition(latLng, 16.0)
             }
         mNaverMap.moveCamera(cameraUpdate)
+    }
+
+    private fun getMyKakaoAccountInfoAndSetProfileView() {
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                ToastUtil.toastShort("사용자 정보 요청 실패")
+            }
+            else if (user != null) {
+                navigation_left.profile_name.text = user.kakaoAccount?.profile?.nickname
+
+                Glide.with(this)
+                    .load(user.kakaoAccount?.profile?.thumbnailImageUrl)
+                    .placeholder(R.mipmap.ic_launcher)
+                    .into(navigation_left.profile_image)
+            }
+        }
+    }
+
+    // Left Drawer 열었을 때, 관련 데이터가 바로 보일 수 있도록, MapFragment 진입시 동작하게 했습니다.
+    // 명준님께서 작업하신 기타 Init 작업이 있으시다면 같이 묶어도 괜찮습니다!
+    private fun dataInit() {
+        getMyKakaoAccountInfoAndSetProfileView()
     }
 }
